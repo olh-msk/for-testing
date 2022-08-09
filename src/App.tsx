0d9597954cc225a3fc04429da7375b6cbf72fc26
Oleh <olh.masiuk@gmail.com>
2022-08-09 18:18:41 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and hello my little world
         </a>
       </header>
     </div>
